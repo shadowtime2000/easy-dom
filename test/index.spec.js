ad772b2abeaa711d469a42fa8cf5d1b0a2f9b41a
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-10-03 17:53:47 -0700

    JSDom Testing

---
+++ test/index.spec.js
@@ -0,0 +1,22 @@
+const { expect } = require("chai");
+const { JSDOM } = require("jsdom");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+
+const { document, window } = new JSDOM("<!DOCTYPE html><html></html>", {
+  runScripts: "dangerously",
+});
+
+describe("#easy-dom", () => {
+  before(() => {
+    window.eval(readFileSync(join(__dirname, "../src/index.js")).toString());
+  });
+
+  it("should exist", () => {
+    expect(window.DOM).to.not.equal(undefined, "window.DOM does not exist");
+  });
+
+  it("should be typeof object", () => {
+    expect(typeof window.DOM === "object", "window.DOM is not an object");
+  });
+});
